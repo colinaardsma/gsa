11d7576cc32e4c11586cf3b9e207387557834ece
colinaardsma <colin.aardsma@gmail.com>
2018-03-02 21:50:27 -0600

    updates for aws

--- staging_deploy.sh
+++ staging_deploy.sh
@@ -9,7 +9,7 @@ echo ...PREPARING FOR DEPLOYMENT TO STAGING...
 #python3 manage.py collectstatic
 
 echo ...SETTING ENVIRONMENT TO STAGING...
-ln -s -f ./gsa/settings.staging.py ./gsa/settings.py
+cat ./gsa/settings.staging.py > ./gsa/settings.py
 
 # gsa-dev is the name of the environment
 echo ...DEPLOYING...
@@ -17,5 +17,5 @@ eb deploy gsa-dev
 
 # this needs to be the last line to set everything back to the dev env
 echo ...SETTING ENVIRONMENT BACK TO DEVELOPMENT...
-ln -s -f ./gsa/settings.development.py ./gsa/settings.py
+cat ./gsa/settings.development.py > ./gsa/settings.py
 
