29931f004341ceac5641081d4b00026bb2828df9
colinaardsma <colin.aardsma@gmail.com>
2018-03-03 21:53:50 -0600

    DEPLOY COMMIT 2018-03-03 21:53:50

--- staging_deploy.sh
+++ staging_deploy.sh
@@ -3,28 +3,35 @@
 
 # run using ./staging_deploy.sh from gsa folder
 
+echo
 echo ...PREPARING FOR DEPLOYMENT TO STAGING...
 
 #echo ...COLLECTING STATIC FILES...
 #python3 manage.py collectstatic
 
+echo
 echo ...SETTING ENVIRONMENT TO STAGING...
 cat ./gsa/settings.staging.py > ./gsa/settings.py
 
+echo
 echo ...ADDING CHANGES TO COMMIT...
 git add .
 
+echo
 echo ...COMMITTING CHANGES...
 git commit -m "DEPLOY COMMIT `date +'%Y-%m-%d %H:%M:%S'`"
 
+echo
 echo ...PUSHING COMMIT...
 git push
 
 # gsa-dev is the name of the environment
+echo
 echo ...DEPLOYING...
 eb deploy gsa-dev
 
 # this needs to be the last line to set everything back to the dev env
+echo
 echo ...SETTING ENVIRONMENT BACK TO DEVELOPMENT...
 cat ./gsa/settings.development.py > ./gsa/settings.py
 
