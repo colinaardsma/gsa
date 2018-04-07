8430d3b4ddc79df40c4288eb3fbefa6e16e09fcd
colin aardsma <colin.aardsma@gmail.com>
2018-04-07 10:23:24 -0500

    updated prod db settings

--- gsa/settings.production.py
+++ gsa/settings.production.py
@@ -20,12 +20,12 @@ ALLOWED_HOSTS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['RDS_DB_NAME'],
-        'USER': os.environ['RDS_USERNAME'],
-        'PASSWORD': os.environ['RDS_PASSWORD'],
-        'HOST': os.environ['RDS_HOSTNAME'],
-        'PORT': os.environ['RDS_PORT'],
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "gsa",
+        "USER": "gsa_admin",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
